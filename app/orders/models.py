7918a5fb431d7e80541d8f886233a9aa926af410
boniek.junior <boniek.junior.123@gmail.com>
2026-05-17 18:22:15 -0300

    docs: Comentando código

--- app/orders/models.py
+++ app/orders/models.py
@@ -4,7 +4,9 @@ from datetime import datetime, timezone
 
 from app.core.database import Base
 
-# 
+# Define os modelos de pedido e item de pedido, que representam as tabelas "orders" e "order_items" no banco de dados. Cada instância dessas classes corresponde a um registro nas respectivas tabelas. O modelo de pedido inclui informações sobre o usuário, status, total e data de criação, enquanto o modelo de item de pedido inclui informações sobre o produto, quantidade e preço. Ambos os modelos possuem relacionamentos com outros modelos para facilitar consultas e operações relacionadas a pedidos.
+
+# Modelo de pedido
 class Order(Base):
     __tablename__ = "orders"
 
@@ -17,7 +19,7 @@ class Order(Base):
     user = relationship("User", back_populates="orders")
     items = relationship("OrderItem", back_populates="order", cascade="all, delete-orphan")
 
-
+# Modelo de item do pedido
 class OrderItem(Base):
     __tablename__ = "order_items"
 
